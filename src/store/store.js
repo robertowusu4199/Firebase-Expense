c301c8a14936530f9c4392e363d000f4cba88b89
robertowusu <robertowusu4199@gmail.com>
2021-07-08 20:37:07 -0800

    adding firebase

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose, } from "redux";
 import ExpenseApp from "../Reducers/ExpenseReducer";
+import firebase from '../firebase/config'
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import thunk from 'redux-thunk'
 
 
-const store = createStore(ExpenseApp)
+const store = createStore(ExpenseApp, compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+    reactReduxFirebase(firebase),
+    reduxFirestore(firebase)
+    ));
 
 export default store;
